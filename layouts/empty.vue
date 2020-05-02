1bce15d2b375a32e9c71068b5535ba9bfa2fa228
DemEvo <urainmq@gmail.com>
2020-05-02 14:27:47 +0300

    nd.  init

---
+++ layouts/empty.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="empty">
+        <h1>empty</h1>
+    </div>
+</template>
+
+<script>
+    // import name from "@/components/name"
+    export default {
+        name: "empty",
+        props: [],
+        components: {},
+        data() {
+            return {}
+        },
+        computed: {},
+        methods: {},
+        mounted() {
+        },
+    }
+</script>
+
+<style lang="scss">
+    .empty {
+
+    }
+</style>
