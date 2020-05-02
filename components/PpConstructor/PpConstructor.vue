476e71fbfd7a980b1266c1f87a4c1b8773c3e4ef
DemEvo <urainmq@gmail.com>
2020-05-02 16:06:33 +0300

    nd.  workFile develop2

--- components/PpConstructor/PpConstructor.vue
+++ components/PpConstructor/PpConstructor.vue
@@ -1,15 +1,6 @@
 <template>
     <div class="PpConstructor">
-        <button @click="saves(file.content, file.fileName, 'txt')">Сохранить</button>
-        <button>
-            <label class="add-item" for="id-input-file-2" style="margin-bottom: 0">
-                <input type="file" class="d-none" id="id-input-file-2"
-                       value=""
-                       accept=".txt"
-                       @change.prevent="loadFile($event)">
-                Загрузить
-            </label>
-        </button>
+
         <div class="pp-panel mt-4" v-if="!!currentNode">
             <div class="attr-panel">
                 <div class="head">{{'Редактор атрибутов'}}</div>
@@ -56,7 +47,6 @@
                 </div>
             </div>
         </div>
-        <div>{{file}}</div>
     </div>
 </template>
 
@@ -80,7 +70,6 @@
 //                    {value:'randList', title:'Вероятностный список'},
 ////                    {value:'linearList', title:'Простой список'},
 //                ],
-                file: null,
             }
         },
         computed: {
@@ -99,49 +88,6 @@
             },
         },
         methods: {
-            saves(data, filename, type) {
-                let file = new Blob([data], {type: type});
-                if (window.navigator.msSaveOrOpenBlob) // IE10+
-                    window.navigator.msSaveOrOpenBlob(file, filename);
-                else { // Others
-                    let a = document.createElement("a"),
-                        url = URL.createObjectURL(file);
-                    a.href = url;
-                    a.download = filename;
-                    document.body.appendChild(a);
-                    a.click();
-                    setTimeout(function() {
-                        document.body.removeChild(a);
-                        window.URL.revokeObjectURL(url);
-                    }, 0);
-                }
-            },
-            loadFile(event) {
-                console.log('processFile::event.target =', event.target);
-                let file = event.target.files[0];
-                console.log('processFile::event.target.files[0] =', file);
-                let content;
-                // if (file.text) {
-                //     file.text().then(v => {
-                //         console.log(v.toString());
-                //
-                //        content = v.toString();
-                //         this.addTab({content, fileName: file.name});
-                //     });
-                // } else {
-                //     content = file.toString();
-                //     console.log('content =',content);
-                //     this.addTab({content, fileName: file.name});
-                // }
-
-                let reader = new FileReader();
-                reader.onload = () => {
-                    content = reader.result;
-                    console.log('content (FR) =', content);
-                    this.file = {content, fileName: file.name};
-                };
-                reader.readAsText(file);
-            },
             reffreshAttrs(key, value) {
                 console.log("reffreshAttrs::", arguments);
                 this.currentNode.attrs = {...this.currentNode.attrs, [key]: value}
--- pages/procs.vue
+++ pages/procs.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="ProcessList">
         <h2>ProcessList</h2>
+        <button @click="saves(file.content, file.fileName, 'txt')">Сохранить</button>
+        <button>
+            <label class="add-item" for="id-input-file-2" style="margin-bottom: 0">
+                <input type="file" class="d-none" id="id-input-file-2"
+                       value=""
+                       accept=".txt"
+                       @change.prevent="loadFile($event)">
+                Загрузить
+            </label>
+        </button>
+        <div>{{file}}</div>
     </div>
 </template>
 
@@ -13,10 +24,56 @@
         components: {},
         props: [],
         data() {
-            return {}
+            return {
+                file: null,
+            }
         },
         computed: {},
-        methods: {},
+        methods: {
+            saves(data, filename, type) {
+                let file = new Blob([data], {type: type});
+                if (window.navigator.msSaveOrOpenBlob) // IE10+
+                    window.navigator.msSaveOrOpenBlob(file, filename);
+                else { // Others
+                    let a = document.createElement("a"),
+                        url = URL.createObjectURL(file);
+                    a.href = url;
+                    a.download = filename;
+                    document.body.appendChild(a);
+                    a.click();
+                    setTimeout(function() {
+                        document.body.removeChild(a);
+                        window.URL.revokeObjectURL(url);
+                    }, 0);
+                }
+            },
+            loadFile(event) {
+                console.log('processFile::event.target =', event.target);
+                let file = event.target.files[0];
+                console.log('processFile::event.target.files[0] =', file);
+                let content;
+                // if (file.text) {
+                //     file.text().then(v => {
+                //         console.log(v.toString());
+                //
+                //        content = v.toString();
+                //         this.addTab({content, fileName: file.name});
+                //     });
+                // } else {
+                //     content = file.toString();
+                //     console.log('content =',content);
+                //     this.addTab({content, fileName: file.name});
+                // }
+
+                let reader = new FileReader();
+                reader.onload = () => {
+                    content = reader.result;
+                    console.log('content (FR) =', content);
+                    this.file = {content, fileName: file.name};
+                };
+                reader.readAsText(file);
+            },
+        },
         mounted(){
         },
     }
